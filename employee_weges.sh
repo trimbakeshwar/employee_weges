8365bace15f15983e5dd72941d2b32557b099091
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-18 20:49:20 +0530

    Added:-calculate employee_weges

--- employee_weges.sh
+++ employee_weges.sh
@@ -1,12 +1,24 @@
+#!/bin/bash 
 echo "welcome to the employee weges"
 function check_attendance(){
 attendance=$((RANDOM%2))
 if [ $attendance -eq 1 ]
 then
-	echo 1
+	echo "1"
 else
-	echo 0
+	echo "0"
 fi
 }
-check_attendance
+result="$( check_attendance $(()) )"
+function check_daily_weges(){
 
+if [ $result -eq 1 ]
+then
+	total_weges=$((20*8))
+	echo $total_weges
+else
+	echo "employee is absent"
+fi
+
+}
+check_daily_weges
