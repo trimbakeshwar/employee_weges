e22ba07117140b724afd3ac5862c4ec5b0a6ae90
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 11:08:11 +0530

    Added:-write a code in function

--- employee_weges.sh
+++ employee_weges.sh
@@ -26,12 +26,11 @@ then
 fi
 }
 function check_daily_weges(){
-result="$( getHours $(()) )"
+	result="$( getHours $(()) )"
 	daily_weges=$((20*result))
 	echo $daily_weges 
 
 }
-#result="$( check_daily_weges $(()) )"
 function calculate_weges_for_month(){
 day=0
 hour=0
@@ -40,11 +39,10 @@ total_sal=1
 while [ $day -le 20 ] || [ $hour -le 100 ]
 do
 	hour="$( getHours $(()) )"
-	 calhour=$(($calhour + hour))
+	calhour=$(($calhour + hour))
 	result="$( check_daily_weges $(()) )"
 	total_sal=$(( $total_sal + result ))
 	((day++))
-
 done
 	echo "$total_sal"
 }
