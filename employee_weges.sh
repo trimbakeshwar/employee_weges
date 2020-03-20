27fe2b5f82bb5d9e1fd082b16e97802f26fb539c
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 11:59:43 +0530

    Added:-store daily weges

--- employee_weges.sh
+++ employee_weges.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 echo "welcome to the employee weges"
+
+
 function check_attendance(){
 attendance=$((RANDOM%2))
 case $attendance in
@@ -10,12 +12,14 @@ case $attendance in
 	echo "0" ;;
 esac
 }
+
+
 function getHours(){
 result="$( check_attendance $(()) )"
 if [ $result -eq 1 ]
 then
-	result=$((RANDOME%2))
-	case $result in 
+	result1=$((RANDOM%2))
+	case $result1 in 
 	1)
 		hour=8;
 		echo $hour ;;
@@ -25,25 +29,35 @@ then
 	esac
 fi
 }
+
+
 function check_daily_weges(){
 	result="$( getHours $(()) )"
 	daily_weges=$((20*result))
 	echo $daily_weges 
 
 }
+
+
 function calculate_weges_for_month(){
 day=0
 hour=0
 calhour=0
 total_sal=1
-while [ $day -le 20 ] || [ $hour -le 100 ]
+while [ $day -lt 20 ] || [ $hour -lt 100 ]
 do
 	hour="$( getHours $(()) )"
 	calhour=$(($calhour + hour))
 	result="$( check_daily_weges $(()) )"
 	total_sal=$(( $total_sal + result ))
+	store[day]=$result
 	((day++))
+	if [ $day -eq 20 ]
+	then
+		break;
+	fi
 done
 	echo "$total_sal"
+	echo ${store[@]}
 }
 calculate_weges_for_month
